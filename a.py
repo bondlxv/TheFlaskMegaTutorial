515a9909063eb16ed0996cc8137d2ca4ce3fe05c
My Name <my@email.com>
2021-11-14 19:08:31 +0100

    aaa

--- a.py
+++ a.py
@@ -1,5 +1,8 @@
 for x in range(10):
-    print(x, x**2)
+    print(x, end="")
+    for y in range(2):
+        print("---" + str(y) , end="")
+    print()
 """
 git config --global user.email "my@email.com"
 git config --global user.name "My Name"
