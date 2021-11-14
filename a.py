c73f2a0ee3dce9956b66da72a724e42aa9f92584
My Name <my@email.com>
2021-11-14 19:23:07 +0100

    ppp

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 for x in range(10):
     print(x, end="")
     for y in range(2):
-        print("---" + str(y) , end="")
+        print("---" + str(y) , end="@@@")
     print()
 """
 git config --global user.email "my@email.com"
